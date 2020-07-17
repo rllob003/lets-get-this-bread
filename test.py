10375f8faab2bc9c2f5a7202196302c2b100c566
ajasm002 <35610471+ajasm002@users.noreply.github.com>
2020-07-17 10:36:00 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-x=5
-y=3
-print("hello world",x,y)
+import cv2
+
+cap = cv2.VideoCapture(0);
+
+while (True):
+    ret, frame = cap.read()
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
