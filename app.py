8e71ee823860bd5b891df0b637b48866f2a61fb8
Randy Llobera <rllob003@fiu.edu>
2020-07-27 18:12:10 -0400

    rewrite static working

--- app.py
+++ app.py
@@ -1,115 +1,76 @@
 from functools import wraps
 import json
 from os import environ as env
-from werkzeug.exceptions import HTTPException
 
-from dotenv import load_dotenv, find_dotenv
-from flask import Flask
+# from werkzeug.exceptions import HTTPException
+
+# from dotenv import load_dotenv, find_dotenv
+from flask import Flask, render_template, url_for, flash, redirect
 from flask import jsonify
-from flask import redirect
-from flask import render_template
 from flask import session
-from flask import url_for
 from flask import send_from_directory
 from authlib.integrations.flask_client import OAuth
 from six.moves.urllib.parse import urlencode
+from forms import LoginForm
 
-import constants
+MESSAGE = ""
 
-ENV_FILE = find_dotenv()
-if ENV_FILE:
-    load_dotenv(ENV_FILE)
+# ENV_FILE = find_dotenv()
+# if ENV_FILE:
+#     load_dotenv(ENV_FILE)
 
-AUTH0_CALLBACK_URL = env.get(constants.AUTH0_CALLBACK_URL)
-AUTH0_CLIENT_ID = env.get(constants.AUTH0_CLIENT_ID)
-AUTH0_CLIENT_SECRET = env.get(constants.AUTH0_CLIENT_SECRET)
-AUTH0_DOMAIN = env.get(constants.AUTH0_DOMAIN)
-AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
-AUTH0_AUDIENCE = env.get(constants.AUTH0_AUDIENCE)
 
-app = Flask(__name__, static_url_path='/public', static_folder='./public')
-app.secret_key = constants.SECRET_KEY
+app = Flask(__name__, static_url_path="/public", static_folder="./public")
 app.debug = True
+app.config["SECRET_KEY"] = "77abd6e0f451012576581201d898304d"
 
 
 @app.errorhandler(Exception)
 def handle_auth_error(ex):
     response = jsonify(message=str(ex))
-    response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
+    # response.status_code = ex.code if isinstance(ex, HTTPException) else 500
     return response
 
 
-oauth = OAuth(app)
-
-auth0 = oauth.register(
-    'auth0',
-    client_id=AUTH0_CLIENT_ID,
-    client_secret=AUTH0_CLIENT_SECRET,
-    api_base_url=AUTH0_BASE_URL,
-    access_token_url=AUTH0_BASE_URL + '/oauth/token',
-    authorize_url=AUTH0_BASE_URL + '/authorize',
-    client_kwargs={
-        'scope': 'openid profile email',
-    },
-)
-
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        if constants.PROFILE_KEY not in session:
-            return redirect('/login')
-        return f(*args, **kwargs)
-
-    return decorated
-
-
 # Controllers API
-@app.route('/')
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/callback')
-def callback_handling():
-    auth0.authorize_access_token()
-    resp = auth0.get('userinfo')
-    userinfo = resp.json()
-
-    session[constants.JWT_PAYLOAD] = userinfo
-    session[constants.PROFILE_KEY] = {
-        'user_id': userinfo['sub'],
-        'name': userinfo['name'],
-    }
-    return redirect('/dashboard')
-
-
-@app.route('/login')
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@xtremer.com" and form.password.data == "admin@123":
+            return redirect(url_for("dashboard"))
+        else:
+            global MESSAGE
+            MESSAGE = "Invalid credentials"
+            return redirect(url_for("home"), MESSAGE)
+    return render_template("login.html", form=form)
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.clear()
-    params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
-    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+    return redirect(url_for("home"))
 
 
-@app.route('/dashboard')
-@requires_auth
+@app.route("/dashboard")
 def dashboard():
-    return render_template('dashboard.html',
-                           userinfo=session[constants.PROFILE_KEY],
-                           userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4))
+    return render_template("dashboard.html")
 
-@app.route('/video')
+
+@app.route("/video")
 def video():
-    return redirect('videotest.html')
+    return redirect("videotest.html")
 
-@app.route('/stream')
+
+@app.route("/stream")
 def stream():
-    return render_template('stream.html')
+    return render_template("stream.html")
 
 
 if __name__ == "__main__":
---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
