5267bf95caf9628a5b58a192923d6a75f969f31c
Gabs <gacos026@fiu.edu>
2020-07-27 14:58:25 -0400

    yrs

--- app.py
+++ app.py
@@ -91,7 +91,8 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    params = {'returnTo': url_for('home', _external=True), 'client_id': AUTH0_CLIENT_ID}
+    params = {'returnTo': url_for(
+        'home', _external=True), 'client_id': AUTH0_CLIENT_ID}
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 
